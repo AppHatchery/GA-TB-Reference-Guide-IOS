a61a5146f3d170ad69522511c103f7d0889244c4
Morgan Greenleaf <morgan.greenleaf@emory.edu>
2021-08-06 22:32:11 -0400

    Initial Commit

---
+++ GA-TB-Reference-Guide/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GA-TB-Reference-Guide
+//
+//  Created by Morgan Greenleaf on 8/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GA-TB-Reference-Guide/GA_TB_Reference_GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  GA_TB_Reference_GuideApp.swift
+//  GA-TB-Reference-Guide
+//
+//  Created by Morgan Greenleaf on 8/6/21.
+//
+
+import SwiftUI
+
+@main
+struct GA_TB_Reference_GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
