fa961cee0837393f751c5171ddaffd4a42806402
yago1994 <yagoaral@hotmail.com>
2023-08-21 12:42:03 -0400

    updated Pendo account; updated Google plist;

--- GA-TB-Reference-Guide/AppDelegate/AppDelegate.swift
+++ GA-TB-Reference-Guide/AppDelegate/AppDelegate.swift
@@ -29,7 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             UserDefaults.standard.set(visitorId, forKey: "visitorId")
         }
 
-        let accountId = "Test"
+        let accountId = "GTRG"
         
         if let visitorId = UserDefaults.standard.string(forKey: "visitorId") {
             // Use visitorID in your Pendo initialization code here
